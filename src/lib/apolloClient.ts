91a1774a7fc070d8274c9349e703b80e760add92
stefano-mazziotta <mazziottastefano@gmail.com>
2023-05-08 09:07:56 -0300

    format file

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -5,12 +5,12 @@ let apolloClient: ApolloClient<any> | null = null;
 export const getApolloClient = () => {
   // create a new client if there's no existing one
   // or if we are running on the server.
-  if ( !apolloClient || typeof window === "undefined" ) {
+  if (!apolloClient || typeof window === "undefined") {
     apolloClient = new ApolloClient({
-        link: new HttpLink({
-            uri:"http://localhost:3000/graphql" // 
-        }),
-        cache: new InMemoryCache(),
+      link: new HttpLink({
+        uri: "http://localhost:3000/graphql" // 
+      }),
+      cache: new InMemoryCache(),
     });
   }
 
