d1c788e3c40556a31d428188a65182a2e7f17c41
stefano-mazziotta <mazziottastefano@gmail.com>
2023-05-15 09:14:43 -0300

    dynamic domain - production or local

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
     experimental: {
         appDir: true,
     },
-    env: {
-        NEXT_PUBLIC_BASE_URL: process.env.NODE_ENV === 'production' ? 'http://steff-analog-a2ynygtlu-stefano-mazziotta.vercel.app' : 'http://localhost:3000'
-    }
+    // env: {
+    //     NEXT_PUBLIC_BASE_URL: process.env.NODE_ENV === 'production' ? 'http://steff-analog-a2ynygtlu-stefano-mazziotta.vercel.app' : 'http://localhost:3000'
+    // }
 }
 
 module.exports = nextConfig
