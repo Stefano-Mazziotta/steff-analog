9172c119abbf6b8f640215a6c8d3978a773b296b
stefano-mazziotta <mazziottastefano@gmail.com>
2023-05-15 11:40:38 -0300

    apollo - embedded sandbox

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { EmbeddedSandbox } from '@/components/EmbeddedSandbox'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -12,6 +13,7 @@ export default function RootLayout( { children, }: { children: React.ReactNode }
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <EmbeddedSandbox />
     </html>
   )
 }
