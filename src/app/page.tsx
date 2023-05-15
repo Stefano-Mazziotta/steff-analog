4ae141c5df28f3a0ca2b1c3063a141b417e42217
stefano-mazziotta <mazziottastefano@gmail.com>
2023-05-15 09:04:26 -0300

    dynamic domain - production or local

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 import { getApolloClient } from "@/lib/apolloClient"
 import { gql } from "@apollo/client"
 
-const query = gql`
-  query 
-    ExampleQuery {
-      hello
-    }
-`
+// const query = gql`
+//   query 
+//     ExampleQuery {
+//       hello
+//     }
+// `
 export default async function HomePage() {
   const apolloClient = getApolloClient();
-  const { data } = await apolloClient.query({ query })
+  // const { data } = await apolloClient.query({ query })
   return (
     <main>
-      <h1>{data.hello}</h1>
+      <h1>hello world</h1>
     </main>
   )
 }
