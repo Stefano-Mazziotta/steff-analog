94ceb7db929bce5861fdcdb743d0d641b0c35571
stefano-mazziotta <mazziottastefano@gmail.com>
2023-05-15 08:35:00 -0300

    test deploy to vercel server

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,77 +1,85 @@
 export const typeDefs = `#graphql
 
-    type Country {
-      id: ID!
-      name: String!
-    }
+  type Country {
+    id: ID!
+    name: String!
+  }
 
-    type Film {
-      id: ID!
-      name: String!
-      createdYear: Int!
-      madeIn: Country
-    }
+  type Film {
+    id: ID!
+    name: String!
+    createdYear: Int!
+    madeIn: Country
+  }
 
-    type Camera {
-      id: ID!
-      name: String!
-      createdYear: Int!
-      madeIn: Country
-    }
+  type Camera {
+    id: ID!
+    name: String!
+    createdYear: Int!
+    madeIn: Country
+  }
 
-    type City {
-      id: ID!
-      name: String!
-      country: Country!
-    }
+  type City {
+    id: ID!
+    name: String!
+    country: Country!
+  }
 
-    type Location {
-      id: ID!
-      name: String!
-      city: City!
-    }
+  type Location {
+    id: ID!
+    name: String!
+    city: City!
+  }
 
-    type Category {
-      id: ID!
-      name: String!
-    }
+  type Category {
+    id: ID!
+    name: String!
+  }
 
-    type Quality {
-      id: ID!,
-      name: String!
-    }
+  type Quality {
+    id: ID!,
+    name: String!
+  }
 
-    type Src {
-      id: ID!,
-      url: String!,
-      quality: Quality!
-      # photo: Photo
-    }
+  type Src {
+    id: ID!,
+    url: String!,
+    quality: Quality!
+    # photo: Photo
+  }
 
-    type Photo {
-      id: ID!
-      height: Int!
-      width: Int!
-      description: String!
-      shootDate: String!
-      film: Film!
-      camera: Camera!
-      location: Location!
-      category: Category!
-      src: [Src!]!
-      createdTimestamp: Int!
-    }
+  type Photo {
+    id: ID!
+    height: Int!
+    width: Int!
+    title: String!
+    description: String!
+    shootDate: String!
+    film: Film!
+    camera: Camera!
+    location: Location!
+    category: Category!
+    src: [Src!]!
+    createdTimestamp: Int!
+  }
 
-    type Query { 
-      getPhotos: [Photo!]!
-      getPhotoById(id: ID!): Photo
-    }
-    type Mutation {
-      addPhoto(input: AddPhotoInput!): String!
-    }
+  type Query { 
+    getPhotos: [Photo!]!
+    getPhotoById(id: ID!): Photo
+  }
+  type Mutation {
+    addPhoto(input: AddPhotoInput!): String!
+  }
 
-    input AddPhotoInput {
-      title: String!
-      description: String!
-    }
+  input AddPhotoInput {
+    height: Int!
+    width: Int!
+    title: String!
+    description: String!
+    shootDate: String!
+    filmId: ID!
+    cameraId: ID!
+    locationId: ID!
+    categoryId: ID!
+  }
 `;
