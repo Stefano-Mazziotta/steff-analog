7f6fa68f60129440a85e5311dae8f5f563b7552e
stefano-mazziotta <mazziottastefano@gmail.com>
2023-05-17 23:49:05 -0300

    add cors

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,22 +1,62 @@
-import { resolvers } from '@/graphql/resolvers'
-import { typeDefs } from '@/graphql/schema'
-import { ApolloServer } from '@apollo/server'
-import { startServerAndCreateNextHandler } from '@as-integrations/next'
-import { makeExecutableSchema } from '@graphql-tools/schema'
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
+import { ApolloServer } from '@apollo/server';
+import Cors, { CorsOptions } from 'cors';
 
-export const schema = makeExecutableSchema({ typeDefs, resolvers })
+import { resolvers } from '@/graphql/resolvers';
+import { typeDefs } from '@/graphql/schema';
 
-const server = new ApolloServer({
-  schema,
-})
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
 
-const handler = startServerAndCreateNextHandler<NextRequest>(server, { context: async req => ({ req }) });
+const whitelist = ['http://localhost:3000', 'https://studio.apollographql.com'];
 
-export async function GET(request: NextRequest) {
-  return handler(request);
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+
+    if (origin === undefined) {
+      callback(null, false);
+      return;
+    }
+
+    const isAllowedOrigin = whitelist.indexOf(origin) !== -1;
+
+    if (!isAllowedOrigin) {
+      callback(new Error('Not allowed by CORS'));
+      return;
+    }
+
+    callback(null, true);
+  },
+  methods: ['POST', 'GET', 'HEAD'],
+};
+
+const cors = Cors(corsOptions);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+const server = new ApolloServer({ schema });
+
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+  context: async (req) => ({ req }),
+});
+
+function runMiddleware(req: NextRequest, res: NextResponse, fn: any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        reject(result);
+      }
+
+      resolve(result);
+    });
+  });
 }
 
-export async function POST(request: NextRequest) {
-  return handler(request);
+export async function GET(req: NextRequest, res:NextResponse) {
+  await runMiddleware(req, res, cors);
+  return handler(req);
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  await runMiddleware(req, res, cors);
+  return handler(req);
 }
