05afcf4bec49cf675cfbe2a0c2200b8b785b0629
stefano-mazziotta <mazziottastefano@gmail.com>
2023-05-20 21:14:19 -0300

    layout - add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,22 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+import { Header } from '@/components/header'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Steff_Analog',
+  title: 'steff-analog',
   description: 'Photography art gallery',
 }
 
 export default function RootLayout( { children, }: { children: React.ReactNode } ) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
