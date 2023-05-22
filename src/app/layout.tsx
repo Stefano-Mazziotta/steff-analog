ca5d8d8630c0d5009561a3fe8a6bac49b708190e
stefano-mazziotta <mazziottastefano@gmail.com>
2023-05-22 08:55:11 -0300

    add header in global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 
+import { Merriweather } from 'next/font/google'
 import { Header } from '@/components/header'
 
-const inter = Inter({ subsets: ['latin'] })
+const merriweather = Merriweather({
+  weight: ['400','700', '900'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   title: 'steff-analog',
@@ -13,7 +18,7 @@ export const metadata = {
 export default function RootLayout( { children, }: { children: React.ReactNode } ) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={merriweather.className}>
         <Header />
         {children}
       </body>
