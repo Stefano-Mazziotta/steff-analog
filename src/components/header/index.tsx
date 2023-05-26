52054e053dd69554fbc648fc2ff15f0d5f747e11
stefano-mazziotta <mazziottastefano@gmail.com>
2023-05-26 20:42:04 -0300

    header responsive

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,27 @@ import styles from './styles.module.css';
 import Link from 'next/link';
 import { useId } from 'react';
 
+interface ILink {
+    id: string
+    href: string
+    description: string
+}
+
+const links:ILink[] = [
+    {
+        id: '1',
+        href: '/home',
+        description: 'Home'
+    },
+    {
+        id: '2',
+        href: '/gallery',
+        description: 'Gallery'
+    }
+]
+
 export function Header(){
+    
     return (
         <header className={styles.header}>
             <nav className={styles.navigator}>
@@ -11,7 +31,20 @@ export function Header(){
                     <h1>Analog Steff</h1>
                 </Link>
                 <Hamburger />
-                <AsideMenu />                
+                <AsideMenu />       
+                <ul className={styles.navigatorList}>
+                    {
+                        links.map(link => {
+                            return(
+                                <li key={link.id} className={styles.listItem}>
+                                    <Link href={link.href}>
+                                        {link.description}
+                                    </Link>                                    
+                                </li>
+                            )
+                        })
+                    }
+                </ul>         
             </nav>
         </header>
     )
@@ -34,9 +67,18 @@ function AsideMenu() {
     return(
         <aside className={styles.asideMenu}>
             <nav className={styles.asideMenuNavigator}>
-                <ul className={styles.navigatorList}>
-                    <Link className={styles.listItem} href="/home"><li>Home</li></Link>
-                    <Link className={styles.listItem} href="/gallery"><li>Gallery</li></Link>
+                <ul className={`${styles.aside} ${styles.navigatorList}`}>
+                    {
+                        links.map(link => {
+                            return(
+                                <li key={link.id} className={styles.listItem}>
+                                    <Link href={link.href}>
+                                        {link.description}
+                                    </Link>                                    
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
             </nav>
         </aside>
