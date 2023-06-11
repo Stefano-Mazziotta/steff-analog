4369c09619b5b51c622402d389decb7b9bc43d2a
stefano-mazziotta <mazziottastefano@gmail.com>
2023-06-11 20:39:10 -0300

    show photos

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,40 +1,52 @@
 import { getApolloClient } from "@/lib/client/apolloClient"
 import { gql } from "@apollo/client"
-import { Key } from "react"
+import Image from "next/image"
 
-interface IPhotosDTO {
-  id: Key
-  description: String
-  shootDate: String
-}
+import styles from './styles.module.css';
 
 const query = gql`
   query GetPhotos {
     getPhotos {
-      id
-      height
-      width
-      description
-      shootDate
+    id
+    height
+    width
+    description
+    shootDate
+    src {
+      regular {
+        url
+      }      
     }
+  }
   }
 `
 export default async function HomePage() {
   const apolloClient = getApolloClient();
   const { data } = await apolloClient.query({ query })
   
-  const photos:IPhotosDTO[] = data.getPhotos;
+  const photos:IPhoto[] = data.getPhotos;
   
   return (
     <main>
+      <section className={styles.gallery}>
       {photos.map(photo => {
         return (
           <ul key={photo.id}>
+            <li className={styles.photoContainer}>
+              <Image 
+                src={photo.src.regular.url}
+                alt={photo.description} 
+                width={200} 
+                height={200}
+                className={styles.photo}
+              />
+            </li>
             <li>{photo.description}</li>
             <li>{photo.shootDate}</li>
           </ul>
         )
         })}
+      </section>
     </main>
   )
 }
