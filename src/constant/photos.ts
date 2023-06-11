5dc36f8b73f262734ee0f764fc0740bd72f70e79
stefano-mazziotta <mazziottastefano@gmail.com>
2023-06-11 20:38:16 -0300

    add more photos to constant array

--- src/constant/photos.ts
+++ src/constant/photos.ts
@@ -39,48 +39,33 @@ export const photos = [
             id: 1,
             name: "Trips"
         },
-        src:[
-            {
+        src: {
+            raw: {
                 id: 1,
                 url: "http://localhost:3000/gallery/raw/1.jpg",
-                quality: {
-                    id: 1,
-                    name: "raw"
-                }
+                name: "raw"
             },
-            {
+            full: {
                 id: 2,
                 url: "http://localhost:3000/gallery/full/1.jpg",
-                quality: {
-                    id: 2,
-                    name: "full"
-                }
+                name: "full"
             },
-            {
+            regular: {
                 id: 3,
                 url: "http://localhost:3000/gallery/regular/1.jpg",
-                quality: {
-                    id: 3,
-                    name: "regular"
-                }
+                name: "regular"
             },
-            {
+            small: {
                 id: 4,
                 url: "http://localhost:3000/gallery/small/1.jpg",
-                quality: {
-                    id: 4,
-                    name: "small"
-                }
+                name: "small"
             },
-            {
+            thumb: {
                 id: 5,
                 url: "http://localhost:3000/gallery/thumb/1.jpg",
-                quality: {
-                    id: 5,
-                    name: "thumb"
-                }
+                name: "thumb"
             }
-        ],
+        },
         createdTimestamp: 1652116708
     },
     {
@@ -123,51 +108,382 @@ export const photos = [
             id: 1,
             name: "Trips"
         },
-        src:[
-            {
+        src: {
+            raw: {
                 id: 6,
                 url: "http://localhost:3000/gallery/raw/2.jpg",
-                quality: {
-                    id: 1,
-                    name: "raw"
-                }
+                name: "raw"
             },
-            {
+            full: {
                 id: 7,
                 url: "http://localhost:3000/gallery/full/2.jpg",
-                quality: {
-                    id: 2,
-                    name: "full"
-                }
+                name: "full"
             },
-            {
+            regular: {
                 id: 8,
                 url: "http://localhost:3000/gallery/regular/2.jpg",
-                quality: {
-                    id: 3,
-                    name: "regular"
-                }
+                name: "regular"
             },
-            {
+            small: {
                 id: 9,
                 url: "http://localhost:3000/gallery/small/2.jpg",
-                quality: {
-                    id: 4,
-                    name: "small"
-                }
+                name: "small"
+
             },
-            {
+            thumb: {
                 id: 10,
                 url: "http://localhost:3000/gallery/thumb/2.jpg",
-                quality: {
-                    id: 5,
-                    name: "thumb"
-                }
+                name: "thumb"
             }
-        ],
+        },
         createdTimestamp: 1652116708
     },
-]
+    {
+        id: 3,
+        width: 2056,
+        height: 3319,
+        description: "Il David di Michele Angelo",
+        shootDate: "01/11/2022",
+        film: {
+            id: 2,
+            name: "Ilford PAN 100",
+            createdYear: 1999,
+            madeIn: {
+                id: 2,
+                name: "Japan"
+            }
+        },
+        camera: {
+            id: 1,
+            name: "Asahi Pentax K1000",
+            createdYear: 1960,
+            madeIn: {
+                id: 2,
+                name: "Japan"
+            }
+        },
+        location: {
+            id: 4,
+            name: "Galleria della Academia",
+            city: {
+                id: 3,
+                name: "Firenze",
+                country: {
+                    id: "3",
+                    name: "Italy"
+                }
+            }
+        },
+        category: {
+            id: 1,
+            name: "Trips"
+        },
+        src: {
+            raw: {
+                id: 11,
+                url: "http://localhost:3000/gallery/raw/david-1-raw.jpg",
+                name: "raw"
+            },
+            full: {
+                id: 12,
+                url: "http://localhost:3000/gallery/full/david-1-full.jpg",
+                name: "full"
+            },
+            regular: {
+                id: 13,
+                url: "http://localhost:3000/gallery/regular/david-1-regular.jpg",
+                name: "regular"
+            },
+            small: {
+                id: 14,
+                url: "http://localhost:3000/gallery/small/david-1-small.jpg",
+                name: "small"
+            },
+            thumb: {
+                id: 15,
+                url: "http://localhost:3000/gallery/thumb/david-1-thumb.jpg",
+                name: "thumb"
+            }
+        },
+        createdTimestamp: 1652116708
+    },
+    {
+        id: 4,
+        width: 3328,
+        height: 2056,
+        description: "Autumn colors in the park",
+        shootDate: "09/28/2022",
+        film: {
+            id: 2,
+            name: "Ilford PAN 100",
+            createdYear: 1999,
+            madeIn: {
+                id: 2,
+                name: "Japan"
+            }
+        },
+        camera: {
+            id: 1,
+            name: "Asahi Pentax K1000",
+            createdYear: 1960,
+            madeIn: {
+                id: 2,
+                name: "Japan"
+            }
+        },
+        location: {
+            id: 4,
+            name: "Galleria della Academia",
+            city: {
+                id: 3,
+                name: "Firenze",
+                country: {
+                    id: "3",
+                    name: "Italy"
+                }
+            }
+        },
+        category: {
+            id: 1,
+            name: "Trips"
+        },
+        src: {
+            raw: {
+                id: 16,
+                url: "http://localhost:3000/gallery/raw/david-2-raw.jpg",
+                name: "raw"
+            },
+            full: {
+                id: 17,
+                url: "http://localhost:3000/gallery/full/david-2-full.jpg",
+                name: "full"
+            },
+            regular: {
+                id: 18,
+                url: "http://localhost:3000/gallery/regular/david-2-regular.jpg",
+                name: "regular"
+            },
+            small: {
+                id: 19,
+                url: "http://localhost:3000/gallery/small/david-2-small.jpg",
+                name: "small"
+            },
+            thumb: {
+                id: 20,
+                url: "http://localhost:3000/gallery/thumb/david-2-thumb.jpg",
+                name: "thumb"
+            }
+        },
+        createdTimestamp: 1652116708
+    },
+    {
+        id: 5,
+        width: 3344,
+        height: 2111,
+        description: "Teatro del Scalla",
+        shootDate: "15/11/2022",
+        film: {
+            id: 4,
+            name: "Kodak Ektar 100",
+            createdYear: 1998,
+            madeIn: {
+                id: 1,
+                name: "United States"
+            }
+        },
+        camera: {
+            id: 1,
+            name: "Asahi Pentax K1000",
+            createdYear: 1960,
+            madeIn: {
+                id: 2,
+                name: "Japan"
+            }
+        },
+        location: {
+            id: 4,
+            name: "Teatro Alla Scalla",
+            city: {
+                id: 4,
+                name: "Milano",
+                country: {
+                    id: "3",
+                    name: "Italy"
+                }
+            }
+        },
+        category: {
+            id: 1,
+            name: "Trips"
+        },
+        src: {
+            raw: {
+                id: 21,
+                url: "http://localhost:3000/gallery/raw/teatro-milan-raw.jpg",
+                name: "raw"
+            },
+            full: {
+                id: 22,
+                url: "http://localhost:3000/gallery/full/teatro-milan-full.jpg",
+                name: "full"
+            },
+            regular: {
+                id: 23,
+                url: "http://localhost:3000/gallery/regular/teatro-milan-regular.jpg",
+                name: "regular"
+            },
+            small: {
+                id: 24,
+                url: "http://localhost:3000/gallery/small/teatro-milan-small.jpg",
+                name: "small"
+            },
+            thumb: {
+                id: 25,
+                url: "http://localhost:3000/gallery/thumb/teatro-milan-thumb.jpg",
+                name: "thumb"
+            }
+        },
+        createdTimestamp: 1652116708
+    },
+    {
+        id: 6,
+        width: 3000,
+        height: 2000,
+        description: "Duomo Vitro",
+        shootDate: "07/20/2022",
+        film: {
+            id: 2,
+            name: "Ilford PAN 100",
+            createdYear: 1999,
+            madeIn: {
+                id: 2,
+                name: "Japan"
+            }
+        },
+        camera: {
+            id: 1,
+            name: "Asahi Pentax K1000",
+            createdYear: 1960,
+            madeIn: {
+                id: 2,
+                name: "Japan"
+            }
+        },
+        location: {
+            id: 4,
+            name: "Il Duomo Di Milano",
+            city: {
+                id: 3,
+                name: "Milano",
+                country: {
+                    id: "3",
+                    name: "Italy"
+                }
+            }
+        },
+        category: {
+            id: 1,
+            name: "Trips"
+        },
+        src: {
+            raw: {
+                id: 21,
+                url: "http://localhost:3000/gallery/raw/vitro-raw.jpg",
+                name: "raw"
+            },
+            full: {
+                id: 22,
+                url: "http://localhost:3000/gallery/full/vitro-full.jpg",
+                name: "full"
+            },
+            regular: {
+                id: 23,
+                url: "http://localhost:3000/gallery/regular/vitro-regular.jpg",
+                name: "regular"
+            },
+            small: {
+                id: 24,
+                url: "http://localhost:3000/gallery/small/vitro-small.jpg",
+                name: "small"
+            },
+            thumb: {
+                id: 25,
+                url: "http://localhost:3000/gallery/thumb/vitro-thumb.jpg",
+                name: "thumb"
+            }
+        },
+        createdTimestamp: 1652116708
+    },
+    {
+        id: 7,
+        width: 3000,
+        height: 2000,
+        description: "Castello Sforza",
+        shootDate: "07/20/2022",
+        film: {
+            id: 2,
+            name: "Ilford PAN 100",
+            createdYear: 1999,
+            madeIn: {
+                id: 2,
+                name: "Japan"
+            }
+        },
+        camera: {
+            id: 1,
+            name: "Asahi Pentax K1000",
+            createdYear: 1960,
+            madeIn: {
+                id: 2,
+                name: "Japan"
+            }
+        },
+        location: {
+            id: 4,
+            name: "Castello di Sforza",
+            city: {
+                id: 3,
+                name: "Milano",
+                country: {
+                    id: "3",
+                    name: "Italy"
+                }
+            }
+        },
+        category: {
+            id: 1,
+            name: "Trips"
+        },
+        src: {
+            raw: {
+                id: 21,
+                url: "http://localhost:3000/gallery/raw/castelo-sforza-raw.jpg",
+                name: "raw"
+            },
+            full: {
+                id: 22,
+                url: "http://localhost:3000/gallery/full/castelo-sforza-full.jpg",
+                name: "full"
+            },
+            regular: {
+                id: 23,
+                url: "http://localhost:3000/gallery/regular/castelo-sforza-regular.jpg",
+                name: "regular"
+            },
+            small: {
+                id: 24,
+                url: "http://localhost:3000/gallery/small/castelo-sforza-small.jpg",
+                name: "small"
+            },
+            thumb: {
+                id: 25,
+                url: "http://localhost:3000/gallery/thumb/castelo-sforza-thumb.jpg",
+                name: "thumb"
+            }
+        },
+        createdTimestamp: 1652116708
+    }
+];
 
 export const categories = [
     {
