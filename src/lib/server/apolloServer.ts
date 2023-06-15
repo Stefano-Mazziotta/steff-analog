b51557391a40a29796fb8273c7af3ac7624e924a
stefano-mazziotta <mazziottastefano@gmail.com>
2023-06-14 21:13:38 -0300

    improve format error

--- src/lib/server/apolloServer.ts
+++ src/lib/server/apolloServer.ts
@@ -1,21 +1,13 @@
 import { GraphQLFormattedError } from "graphql";
 
-export function formatError(formatedError: GraphQLFormattedError): GraphQLFormattedError {
-    // Check if the error object contains any extensions
-    if (formatedError.extensions && formatedError.extensions.code === "BAD_REQUEST") {
-        // Customize the error message for the specific code
-        const errorMessage = "Bad request. Please check your input.";
-        return {
-            message: errorMessage
-        };
-    }
-
-    // Default error message
-    const errorMessage = "An error occurred. Please try again later.";
+export function formatError(formattedError: GraphQLFormattedError): GraphQLFormattedError {
+  if (formattedError.extensions?.code === "BAD_REQUEST") {
     return {
-        message: errorMessage
+      message: "Bad request. Please check your input."
     };
+  }
+
+  return {
+    message: "An error occurred. Please try again later."
+  };
 }
-
-
-
