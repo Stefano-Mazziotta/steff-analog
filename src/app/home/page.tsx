5d08e3188087d87ad0b4ec16832e796a6439f8ca
stefano-mazziotta <mazziottastefano@gmail.com>
2023-06-15 08:35:02 -0300

    test deploy

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -22,7 +22,7 @@ const query = gql`
 `
 export default async function HomePage() {
   const apolloClient = getApolloClient();
-  // const { data } = await apolloClient.query({ query });
+  const { data } = await apolloClient.query({ query });
   
   // const photos:IPhoto[] = data.getPhotos;
   
