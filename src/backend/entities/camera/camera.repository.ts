3b31293d72dd1e47e3cb0a52ee56beada350f13c
stefano-mazziotta <mazziottastefano@gmail.com>
2023-07-23 19:16:47 -0300

    [backend] create camera's repository and service

---
+++ src/backend/entities/camera/camera.repository.ts
@@ -0,0 +1,16 @@
+import { PrismaClient, Prisma, Camera } from "@prisma/client"
+
+export default class CameraRepository {
+
+    public async create(prisma:PrismaClient, newCamera: Prisma.CameraCreateInput): Promise<Camera>{
+        const createdCamera = await prisma.camera.create({
+            data: newCamera
+        })
+
+        return createdCamera
+    }
+
+    public static createMany(){
+        
+    }
+}
