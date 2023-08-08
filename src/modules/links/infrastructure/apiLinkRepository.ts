85ba71ee4e4d10a7c1e287a74effdbfd89a9153b
stefano-mazziotta <mazziottastefano@gmail.com>
2023-08-07 21:09:07 -0300

    remove unnecessary comment

--- src/modules/links/infrastructure/apiLinkRepository.ts
+++ src/modules/links/infrastructure/apiLinkRepository.ts
@@ -14,22 +14,6 @@ export function createApiLinkRepository(): LinkRepository {
         return createdLink
     }
 
-    // public async create(prisma:PrismaClient, photoId: number, fileName: string): Promise<void> {
-    //     const qualities: Quality[] = await prisma.quality.findMany()
-    //     qualities.forEach(async quality => {
-    //         const { id, name } = quality
-
-    //         await prisma.src.create({
-    //             data: {
-    //                 url: `galery/${name}/${fileName}-${name}`,
-    //                 photoId: photoId,
-    //                 qualityId: id
-    //             }
-    //         })
-
-    //     })
-    // }
-
     async function createMany(newLinks: Prisma.LinkCreateManyInput[] ): Promise<Prisma.BatchPayload> {
         const createdLinks = await prisma.link.createMany({
             data: newLinks
